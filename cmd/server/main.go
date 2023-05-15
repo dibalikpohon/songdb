8de07ae24e2419d37dfd92a126f568749abe79e6
dibalikpohon <dibalikpohon@gmail.com>
2023-05-15 12:29:08 +0700

    apply everything that has been made to main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-  "github.com/labstack/echo/v4"
-  "songdb/pkg/routes"
+	"songdb/pkg/config"
+	"songdb/pkg/controller"
+	"songdb/pkg/routes"
+	"songdb/pkg/service"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
   e := echo.New()
-  routes.RegisterSongRoutes(e)
-  routes.RegisterAlbumRoutes(e)
-  routes.RegisterSongrelRoutes(e)
+  db, err := config.GetDb()
+  if err != nil {
+    panic(err.Error())
+  }
+  defer db.Close()
+
+  // Create all services
+  songService := service.NewSongService(db)
+  albumService := service.NewAlbumService(db)
+  songRelService := service.NewSongRelService(db)
+
+  // Create all controllers
+  songController := controller.NewSongController(songService)
+  albumController := controller.NewAlbumController(albumService)
+  songRelController := controller.NewSongRelController(songRelService)
+
+  // Create all routes
+  songRoutes := routes.NewSongRoutes(songController)
+  albumRoutes := routes.NewAlbumRoutes(albumController)
+  songRelRoutes := routes.NewSongRelRoutes(songRelController)
+
+  // Register all routes to Echo
+  songRoutes.Register(e)
+  albumRoutes.Register(e)
+  songRelRoutes.Register(e)
+
   e.Logger.Fatal(e.Start(":9000"))
 }
