51a2e64a0ee92df4d2a024317de67800abd5a631
dibalikpohon <dibalikpohon@gmail.com>
2023-05-26 20:12:55 +0700

    *Added functions to models*: 1. To Update from dto 2. to convert to entity from dto

--- pkg/models/album.go
+++ pkg/models/album.go
@@ -1,12 +1,25 @@
 package models
 
 type Album struct {
-  Id string `json:"id"`
-  Name string `json:"name"`
-  Year int16 `json:"year"`
+  Id string `json:"id" gorm:"primaryKey"`
+  Name string `json:"name" gorm:"notNull"`
+  Year int16 `json:"year" gorm:"notNull"`
 }
 
 type AlbumDto struct {
   Name string `json:"name"`
   Year int16 `json:"year"` 
+}
+
+func (a *Album) UpdateFromDto(dto AlbumDto) {
+  if len(dto.Name) > 0 {
+    a.Name = dto.Name
+  }
+  if dto.Year > 0 {
+    a.Year = dto.Year
+  }
+}
+
+func (a AlbumDto) ToEntity() Album {
+  return Album{Name: a.Name, Year: a.Year}
 }
--- pkg/models/song.go
+++ pkg/models/song.go
@@ -1,11 +1,11 @@
 package models
 
 type Song struct {
-  Id string         `json:"id"`
-  Title string      `json:"title"`
-  Genre string      `json:"genre"`
+  Id string         `json:"id" gorm:"primaryKey"`
+  Title string      `json:"title" gorm:"notNull"`
+  Genre string      `json:"genre" gorm:"notNull"`
   Duration int32    `json:"duration"`
-  Year int16        `json:"year"`
+  Year int16        `json:"year" gorm:"notNull"`
 }
 
 type SongDto struct { 
@@ -13,4 +13,24 @@ type SongDto struct {
   Genre string      `json:"genre"`
   Duration int32    `json:"duration"`
   Year int16        `json:"year"`
+}
+
+func (s *Song) UpdateFromDto(dto SongDto) {
+  if len(dto.Title) > 0 {
+    s.Title = dto.Title
+  }
+  if len(dto.Genre) > 0 {
+    s.Genre = dto.Genre
+  }
+  if dto.Duration > 0 {
+    s.Duration = dto.Duration
+  }
+  if dto.Year > 0 {
+    s.Year = dto.Year
+  }
+}
+
+func (s SongDto) ToEntity() Song {
+  return Song{Title: s.Title, Genre: s.Genre,
+              Duration: s.Duration, Year: s.Year}
 }
