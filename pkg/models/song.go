75efce34f56f37260b0abe1bc0502da5fcd9a11b
dibalikpohon <dibalikpohon@gmail.com>
2023-05-30 16:30:32 +0700

    add albumId on struct

--- pkg/models/song.go
+++ pkg/models/song.go
@@ -1,12 +1,14 @@
 package models
 
+import "database/sql"
+
 type Song struct {
   Id string         `json:"id" gorm:"primaryKey;size:10"`
   Title string      `json:"title" gorm:"notNull;size:30"`
   Genre string      `json:"genre" gorm:"notNull;size:30"`
   Duration int32    `json:"duration"`
   Year int16        `json:"year" gorm:"notNull"`
-  AlbumId string
+  AlbumId sql.NullString `json:"-"`
 }
 
 type SongDto struct { 
@@ -14,6 +16,7 @@ type SongDto struct {
   Genre string      `json:"genre"`
   Duration int32    `json:"duration"`
   Year int16        `json:"year"`
+  AlbumId string    `json:"albumId"`
 }
 
 func (s *Song) UpdateFromDto(dto SongDto) {
@@ -29,9 +32,20 @@ func (s *Song) UpdateFromDto(dto SongDto) {
   if dto.Year > 0 {
     s.Year = dto.Year
   }
+  if len(dto.AlbumId) > 0 {
+    s.AlbumId.String = dto.AlbumId
+    s.AlbumId.Valid = true
+  }
 }
 
 func (s SongDto) ToEntity() Song {
-  return Song{Title: s.Title, Genre: s.Genre,
-              Duration: s.Duration, Year: s.Year}
+  if len(s.AlbumId) > 0 {
+    return Song{Title: s.Title, Genre: s.Genre,
+                Duration: s.Duration, Year: s.Year, 
+                AlbumId: sql.NullString{Valid: true, String: s.AlbumId}}
+  } else {
+    return Song{Title: s.Title, Genre: s.Genre,
+                Duration: s.Duration, Year: s.Year, 
+                AlbumId: sql.NullString{Valid: false}}
+  }
 }
